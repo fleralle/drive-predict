69c0f871c69538a30b9f651cf5af19332eb04b15
francoisnation <francois@wearenation.co.uk>
2019-07-17 17:41:25 -0500

    Add brake feature metrics extraction

--- models/data.py
+++ models/data.py
@@ -105,10 +105,48 @@ def extract_brake_features(df: pd.DataFrame, treshold=-2, brake_interval=2):
     for i in range(len(boundaries_indexes)):
         min_bound = 0 if i == 0 else boundaries_indexes[i-1]
         max_bound = boundaries_indexes[i]
-        brake_features.append(brake_df[min_bound:max_bound])
+
+        if len(brake_df[min_bound:max_bound]) > 10:
+            brake_features.append(brake_df[min_bound:max_bound])
 
     return brake_features
 
 
-def calculate_feature_metrics(feature_df: pd.DataFrame):
-    pass
+def calculate_distance(origin_coord: np.array, destination_coord: np.array):
+    square_distance = sum((destination_coord - origin_coord)**2)
+    return (square_distance**.5)*10
+
+
+def get_coordinates(observation):
+    # print(observation.velodyne_gps_0, observation.velodyne_gps_1, observation.velodyne_gps_2, '\n')
+    return np.array([
+        observation.velodyne_gps_0,
+        observation.velodyne_gps_1,
+        observation.velodyne_gps_2])
+    # return np.array([
+    #     observation.fiber_compass_x,
+    #     observation.fiber_compass_y,
+    #     observation.fiber_compass_z])
+
+def calculate_brake_distance(feature_df):
+    # print(len(feature_df))
+    origin_obs = feature_df.iloc[0, :]
+    destination_obs = feature_df.iloc[-1, :]
+
+    origin_coord = get_coordinates(origin_obs)
+    destination_coord = get_coordinates(destination_obs)
+
+    # return destination_coord
+    # print(origin_coord, destination_coord)
+    return calculate_distance(origin_coord, destination_coord)
+
+
+def calculate_brake_metrics(features: list):
+    # print(type(feature_df.describe()))
+    metrics = [pd.concat([feature.speed.describe(), feature.car_accel.describe()], axis=0) for feature in features]
+    # brake_distance = [calculate_brake_distance(feature) for feature in features]
+    # max_speed = [feature.speed.max() for feature in features]
+
+    return pd.concat(metrics, axis=1).T.reset_index()
+    # return metrics.T.reset_index()
+    # return pd.concat([pd.Series(brake_distance), pd.Series(max_speed)], axis=1)
