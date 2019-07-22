5f3375b15f6922b2be07204d39f5fe356900424c
francoisnation <francois@wearenation.co.uk>
2019-07-22 15:27:25 -0500

    Refactor functions + add feature distribution plot funct

--- models/data.py
+++ models/data.py
@@ -1,8 +1,10 @@
 """Data util librairy."""
-import numpy as np
 import pandas as pd
 import os
 import fnmatch
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Columns names in CSV file.
 TRIP_DATA_COLUMNS = [
@@ -68,28 +70,31 @@ def load_dataset_as_dataframe(data_dir_path: str):
     return concatenated_df
 
 
-def load_trip_dataset(data_dir_path: str):
-    dataframe_list = []
-    max_time = 0
+def plot_feature_distributions(
+                                df: pd.DataFrame,
+                                figsize: tuple,
+                                features=TRIP_DATA_COLUMNS):
+    """Plot features distributions.
 
-    for filename in os.listdir(data_dir_path):
-        if fnmatch.fnmatch(filename, 'fileID*_ProcessedTripData.csv'):
-            # Load dataset and initialise Dataframe
-            df = pd.read_csv(os.path.join(data_dir_path, filename), header=None)
-            df.columns = TRIP_DATA_COLUMNS
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataset containing the features.
+    figsize : tuple
+        Matplotlib figsize param.
+    features : list
+        List of features.
 
-            # Assign time to keep record continuity
-            df.time = df.time + max_time
-            max_time = df.time.max()
-            dataframe_list.append(df)
+    """
+    nrows = len(features)//2 + 1 if len(features) % 2 else len(features)//2
 
-    # Merge all together
-    concatenated_df = pd.concat(dataframe_list, ignore_index=True)
+    fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=figsize)
+    fig.delaxes(axes[nrows-1][1])  # Removes last empty subplot
 
-    # Assign idx based on new index. Useful to brakedown events later on.
-    concatenated_df['idx'] = concatenated_df.index
-
-    return concatenated_df
+    for i, col_name in enumerate(features):
+        row = i // 2
+        col = i % 2
+        sns.distplot(df[col_name], ax=axes[row][col])
 
 
 def extract_brake_events(df: pd.DataFrame, treshold=-2):
@@ -135,7 +140,6 @@ def extract_brake_features(df: pd.DataFrame, treshold=-2, brake_interval=2):
     brake_df = extract_brake_events(df)
 
     # Measures are taken at a 100Hz frequency.
-    # boundaries = (brake_df.idx - brake_df.idx.shift()) > (brake_interval * 100)
     boundaries = (brake_df.time - brake_df.time.shift()) > (brake_interval)
     new_boundaries = boundaries.reset_index()
 
@@ -152,37 +156,8 @@ def extract_brake_features(df: pd.DataFrame, treshold=-2, brake_interval=2):
     return brake_features
 
 
-def calculate_distance(origin_coord: np.array, destination_coord: np.array):
-    square_distance = sum((destination_coord - origin_coord)**2)
-    return (square_distance**.5)*10
-
-
-def get_coordinates(observation):
-    # print(observation.velodyne_gps_0, observation.velodyne_gps_1, observation.velodyne_gps_2, '\n')
-    return np.array([
-        observation.velodyne_gps_0,
-        observation.velodyne_gps_1,
-        observation.velodyne_gps_2])
-    # return np.array([
-    #     observation.fiber_compass_x,
-    #     observation.fiber_compass_y,
-    #     observation.fiber_compass_z])
-
-def calculate_brake_distance(feature_df):
-    # print(len(feature_df))
-    origin_obs = feature_df.iloc[0, :]
-    destination_obs = feature_df.iloc[-1, :]
-
-    origin_coord = get_coordinates(origin_obs)
-    destination_coord = get_coordinates(destination_obs)
-
-    # return destination_coord
-    # print(origin_coord, destination_coord)
-    return calculate_distance(origin_coord, destination_coord)
-
-
 def calculate_event_metrics(event_df: pd.DataFrame):
-    """Calculates metrics for given driving event.
+    """Calculate metrics for given driving event.
 
     Parameters
     ----------
@@ -204,7 +179,8 @@ def calculate_event_metrics(event_df: pd.DataFrame):
         'pitch',
         'shift'
     ]
-    num_metrics = [event_df[feature].describe().add_prefix(feature + '_') for feature in numerical_features]
+    num_metrics = [event_df[feature].describe().add_prefix(feature + '_')
+                   for feature in numerical_features]
     num_metrics_ds = pd.concat(num_metrics, axis=0)
 
     # Build categorical data metrics
@@ -214,7 +190,8 @@ def calculate_event_metrics(event_df: pd.DataFrame):
         'rain_intensity',
         'driver_wellbeing'
     ]
-    cat_metrics = [event_df[feature].mean() for feature in categorical_features]
+    cat_metrics = [event_df[feature].mean()
+                   for feature in categorical_features]
     cat_metrics_ds = pd.Series(cat_metrics, index=categorical_features)
 
     # Merge numerical and categorical metrics
@@ -230,7 +207,7 @@ def calculate_event_metrics(event_df: pd.DataFrame):
 
 
 def get_events_metrics(events: list):
-    """Returns events metrics dataframe.
+    """Return events metrics dataframe.
 
     Parameters
     ----------
